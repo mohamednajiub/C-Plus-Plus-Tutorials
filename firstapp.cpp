45162c7231d36a217b497d815d07820d00cbcaa0
mohamednajiub <m.emad550@gmail.com>
2019-11-15 20:33:37 +0200

    docs: cin

--- firstapp.cpp
+++ firstapp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main(){
-    int slices = 8;
+    int slices;
+    std::cout << "How many pieces of pizza you have?" << std::endl;
+    std::cin >> slices;
     std::cout << "You have " << slices << " slices of pizza." << std::endl;
-    printf("%i\n", slices);
 }
