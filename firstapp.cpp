96f46b86674a4b6d01b43a5fd2e4ef04903431cc
mohamednajiub <m.emad550@gmail.com>
2019-11-15 20:40:09 +0200

    Rename: change the application name

--- firstapp.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main(){
-    int slices;
-    std::cout << "How many pieces of pizza you have?" << std::endl;
-    std::cin >> slices;
-    std::cout << "You have " << slices << " slices of pizza." << std::endl;
-}
---
+++ pizzaApp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    int slices;
+    std::cout << "How many pieces of pizza do you want?" << std::endl;
+    std::cin >> slices;
+    std::cout << "Here you are! You have " << slices << " slices of pizza." << std::endl;
+}
